acfe07277276e64e18a88dd4b6164cac9a4e1929
Ali Bahadoran <71150321+parsfront@users.noreply.github.com>
2022-10-10 05:11:11 +0000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// DOM => Document Object Model
+// BOM => Browser Object Model
+
+let customScroll = document.querySelector('#scroll')
+
+window.addEventListener('scroll', function () {
+    
+    let scrollTop = window.scrollY
+
+    let documentHeight = document.body.clientHeight
+
+    let windowHeight = window.innerHeight
+
+    let scrollPercent = scrollTop / (documentHeight - windowHeight)
+
+    let scrollPercentRounded = Math.round(scrollPercent * 100)
+
+    customScroll.style.width = scrollPercentRounded + '%'
+
+    console.log(scrollPercentRounded);
+})
